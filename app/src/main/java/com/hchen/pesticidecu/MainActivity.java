625b6a825da993f1c7140c923b9869898e21c13a
Hchen <Hchen_1995@163.com>
2017-07-26 16:50:48 +0800

    listview

--- app/src/main/java/com/hchen/pesticidecu/MainActivity.java
+++ app/src/main/java/com/hchen/pesticidecu/MainActivity.java
@@ -2,6 +2,8 @@ package com.hchen.pesticidecu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] strs = {"基神","B神","翔神","曹神","J神"};
+        //创建ArrayAdapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this,android.R.layout.simple_expandable_list_item_1,strs);
+        //获取ListView对象，通过调用setAdapter方法为ListView设置Adapter设置适配器
+        ListView list_test = (ListView) findViewById(R.id.list_test);
+        list_test.setAdapter(adapter);
     }
 }
