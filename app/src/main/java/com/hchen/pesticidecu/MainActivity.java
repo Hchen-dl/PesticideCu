5f7bf425f0241c2222a8050d51ac522228ff614c
Hchen <Hchen_1995@163.com>
2017-07-28 16:44:16 +0800

    notification

--- app/src/main/java/com/hchen/pesticidecu/MainActivity.java
+++ app/src/main/java/com/hchen/pesticidecu/MainActivity.java
@@ -1,8 +1,11 @@
 package com.hchen.pesticidecu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +21,16 @@ public class MainActivity extends AppCompatActivity {
         //获取ListView对象，通过调用setAdapter方法为ListView设置Adapter设置适配器
         ListView list_test = (ListView) findViewById(R.id.list_test);
         list_test.setAdapter(adapter);
+        Button b=(Button) findViewById(R.id.btn_next);
+        b.setOnClickListener(listener);
     }
+    Button.OnClickListener listener=new Button.OnClickListener()
+    {
+        public void onClick(View v)
+        {
+            Intent intent=new Intent(MainActivity.this,NotificationActivity.class);
+            startActivity(intent);
+            MainActivity.this.finish();
+        }
+    };
 }
