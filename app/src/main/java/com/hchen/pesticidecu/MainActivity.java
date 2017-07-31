c8c75f1f4886e1ba41e3c745a3f69aae195995b2
Hchen <Hchen_1995@163.com>
2017-07-31 19:52:11 +0800

    detect"tmgp.sgame"

--- app/src/main/java/com/hchen/pesticidecu/MainActivity.java
+++ app/src/main/java/com/hchen/pesticidecu/MainActivity.java
@@ -1,19 +1,35 @@
 package com.hchen.pesticidecu;
 
+import android.annotation.TargetApi;
 import android.app.ActivityManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private Context mContext;
+    private NotificationManager mNManager;
+    private Notification notify1;
+    Bitmap LargeBitmap = null;
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,17 +44,19 @@ public class MainActivity extends AppCompatActivity {
         btn_reg.setOnClickListener(reg_listener);
         ActivityManager am=(ActivityManager)this.getSystemService((ACTIVITY_SERVICE));
         List<ActivityManager.RunningAppProcessInfo> apps=am.getRunningAppProcesses();
-        String[] strapps=new String[apps.size()];
 
         for(int n=0;n<apps.size();n++)
         {
-            strapps[n]=apps.get(n).processName;
+            if(apps.get(n).processName.contains("com.tencent.tmgp.sgame"))
+            {
+                mContext = MainActivity.this;
+                //创建大图标的Bitmap
+                LargeBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.lighthouse);
+                mNManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                notify_a();
+                break;
+            };
         }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>
-                (this,android.R.layout.simple_expandable_list_item_1,strapps);
-        ListView listapps=(ListView)findViewById(R.id.list_apps);
-        listapps.setAdapter(adapter);
 
     }
     Button.OnClickListener nex_listener=new Button.OnClickListener()
@@ -59,4 +77,27 @@ public class MainActivity extends AppCompatActivity {
             MainActivity.this.finish();
         }
     };
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public void notify_a()
+    {
+        //Toast.makeText(getApplicationContext(), "默认Toast样式",Toast.LENGTH_SHORT).show();
+
+        Intent it = new Intent(mContext, OtherActivity.class);
+        PendingIntent pit = PendingIntent.getActivity(mContext, 0, it, 0);
+
+        Notification.Builder mBuilder = new Notification.Builder(this);
+        mBuilder.setContentTitle("叶良辰")                        //标题
+                .setContentText("我有一百种方法让你呆不下去~")      //内容
+                .setSubText("——记住我叫叶良辰")                    //内容下面的一小段文字
+                .setTicker("收到叶良辰发送过来的信息~")             //收到信息后状态栏显示的文字信息
+                .setWhen(System.currentTimeMillis())           //设置通知时间
+                .setSmallIcon(R.mipmap.penguins)            //设置小图标
+                .setLargeIcon(LargeBitmap)                     //设置大图标
+                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE)    //设置默认的三色灯与振动器
+                .setSound(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.zhizunbao))  //设置自定义的提示音
+                .setAutoCancel(true)                           //设置点击后取消Notification
+                .setContentIntent(pit);                        //设置PendingIntent
+        notify1 = mBuilder.build();
+        mNManager.notify(1, notify1);
+    }
 }
